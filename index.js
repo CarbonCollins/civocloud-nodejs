22a77118ae48cdda84e9d709ce03aacd6edfc16c
carboncollins <steven@carboncollins.uk>
2017-05-17 17:34:14 +0100

    added base files

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Name:    civocloud-nodejs
+ * Author:  Steven Collins (https://github.com/CarbonCollins)
+ * Date:    Wednesday 17th May 2017
+ *
+ * Git:     https://github.com/CarbonCollins/civocloud-nodejs
+ * Desc:    A module for accessing civo cloud apis
+ */
+
+'use strict';
+
+/**
+ * @class {CivoAPI} 
+ */
+class CivoAPI {
+  /**
+   * @constructor {CivoAPI}
+   * @param {String} apiToken the provided api token from your civo account
+   */
+  constructor(apiToken) {
+
+  }
+}
+
+/**
+ * @type {CivoAPI} module.exports exports the module for use within an application
+ */
+module.exports = CivoAPI;
